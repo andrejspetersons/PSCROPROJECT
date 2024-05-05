47aed651426ac51ee35ac8fd2372ef8d695ccb72
andrejspetersons <andrejpeterson1@gmail.com>
2024-05-05 19:18:51 +0300

    Project base 05.05.24

---
+++ P&K accounting/Form1.cs
@@ -0,0 +1,10 @@
+namespace P_K_accounting
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ P&K payment/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(P_K_payment.Startup))]
+namespace P_K_payment
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
